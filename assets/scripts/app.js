041736a046b195aea72a2a99beb6fe8d3d30813f
EJcodes <IHMDemon.exe@gmail.com>
2020-11-02 13:04:46 -0500

    little change

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,8 +1,9 @@
 const defaultResult = 0;
+
 let currentResult = defaultResult; 
 
-currentResult = currentResult + 10;
+currentResult = (currentResult + 10) * 3/2-1;
 
-let calculationDescription = '(0+10) * 3/2-1';
+let calculationDescription = `( ${defaultResult} + '+ 10 ) * 3/2-1`;
 
-outputResult(currentResult, '');
+outputResult(currentResult, calculationDescription);
